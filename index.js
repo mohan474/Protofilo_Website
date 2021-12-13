5e454d070fe06f279044372ba8b3cd0ff5de72b6
mohan474 <91386074+mohan474@users.noreply.github.com>
2021-12-13 19:51:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+// Common Class List 
+const menu = document.querySelector('.menu');
+const navOpen = document.querySelector('.humburger');
+const navClose = document.querySelector('.close');
+const navBar = document.querySelector('.nav');
+
+// nav List 
+const navLeft = menu.getBoundingClientRect().left;
+
+// Add EventListener  Open nav
+navOpen.addEventListener('click', () =>{
+    if(navLeft < 0){
+        menu.classList.add('show');
+        document.body.classList.add("show");
+        navBar.classList.add("show")
+    }
+});
+
+// Close nav 
+navClose.addEventListener('click', () =>{
+    menu.classList.remove('show');
+    document.body.classList.remove('show');
+    navBar.classList.remove('show')
+});
+
+// Scroll bar with Nav
+
+const navHeigth = navBar.getBoundingClientRect().height;
+
+window.addEventListener('scroll', ()=> {
+    const scrollheight = window.pageYOffset;
+    if (scrollheight > navHeigth){
+        navBar.classList.add('fix-nav');
+    }else{
+        navBar.classList.remove('fix-nav');
+    }
+})
+
+// Scroll To
+
+const links = [...document.querySelectorAll(".scroll-link")];
+
+links.map(link => {
+    if(!link) return;
+    link.addEventListener('click', e => {
+        e.preventDefault();
+
+        const id = e.target.getAttribute('href').slice(1);
+
+        const el = document.getElementById(id);
+        let position = el.offsetTop - navHeigth;
+        window.scrollTo({
+            top: position,
+            left: 0,
+        });
+
+        menu.classList.remove('show');
+        document.body.classList.remove("show");
+        navBar.classList.remove("show")
+    });
+});
+
+gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10});
+gsap.from(".humburger", {opacity: 0, duration: 1, delay: 1, x: 20});
+gsap.from(".hero-img", {opacity: 0, duration: 1, delay: 1.5, x: 200});
+gsap.from(".hero-content h2", {opacity: 0, duration: 1, delay: 2, y: -50});
+gsap.from(".hero-content h1", {opacity: 0, duration: 1, delay: 2.5, y: -45});
+gsap.from("hero-content a", {opacity: 0, duration: 1, delay: 3.5, y: 50});
